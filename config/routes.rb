1ae6f7328fb68fcdedebecb846c7bbe7814eeba3
Alicia Stromback <aliciastromback@gmail.com>
2022-11-17 17:08:42 +0100

    Watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:index, :new, :create]
+  end
+    resources :bookmarks, only: :destroy
+end
+
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new', as: 'new'
+  # get 'lists/:id', to: 'lists#show'
+  # post 'lists', to: 'lists#create'
+
+  # get 'lists/:id/bookmarks', to: 'bookmarks#index'
+  # get 'lists/:id/bookmarks/new' to: 'bookmarks#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 10.times do |movie|
+#   randomnumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].sample
+#   movie = Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: Faker::Internet.url, rating: randomnumber )
+#   puts movie.title
+# end
+
+List.create(name: 'romantic')
+List.create(name: 'comedy')
+List.create(name: 'thriller')
+List.create(name: 'drama')
+List.create(name: 'action')
