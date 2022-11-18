5bfd4ce18b62220be374b33f489774f8ffe8825c
Alicia Stromback <aliciastromback@gmail.com>
2022-11-18 18:15:53 +0100

    Week 6, Day 5, Watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,10 @@ List.create(name: 'comedy')
 List.create(name: 'thriller')
 List.create(name: 'drama')
 List.create(name: 'action')
+
+require "open-uri"
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+list = List.new(title: "NES", body: "A great console")
+list.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+list.save
